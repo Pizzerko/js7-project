79b5c22a6759794221df4a759b08c3ec0cd9b068
Pizzerko <98603569+Pizzerko@users.noreply.github.com>
2023-01-24 01:40:32 -0500

    Add files via upload

--- connect4.js
+++ connect4.js
@@ -13,25 +13,6 @@ window.onload = function() {
     setGame();
 }
 
-function setGame(){
-    board = [];
-    currentColumn = [5, 5, 5, 5, 5, 5, 5];
-    for (let r = 0; r < rows; r++){
-        let row = [];
-        for(let c = 0; c < columns; c++){
-
-            row.push(' ');
-
-            let tile = document.createElement("div");
-            tile.id = r.toString() + "-" + c.toString();
-            tile.classList.add("tile");
-            tile.addEventListener("click", placePiece)
-            document.getElementById("board").append(tile);
-        }
-        board.push(row);
-    }
-}
-
 function placePiece() {
     if(gameOver) return;
 
@@ -59,6 +40,73 @@ function placePiece() {
     cWin();
 }
 
-function cWin(){
+function setGame(){
+    board = [];
+    currentColumn = [5, 5, 5, 5, 5, 5, 5];
+    for (let r = 0; r < rows; r++){
+        let row = [];
+        for(let c = 0; c < columns; c++){
 
+            row.push(' ');
+
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            tile.addEventListener("click", placePiece)
+            document.getElementById("board").append(tile);
+        }
+        board.push(row);
+    }
+}
+
+function setWin(r, c){
+    let winner = document.getElementById("win");
+    if(board[r][c] == playerRed) winner.innerText = "Red Wins!";
+    else{
+        winner.innertext = "Yellow Wins!"
+    }
+    gameOver=true;
+}
+
+function cWin(){
+    for(let r = 0; r < rows; r++){
+        for(let c = 0; c < columns; c++){
+            if(board[r][c] != ' '){
+                if(board[r][c] == board[r][c + 1] && board[r][c+1] == board[r][c+2] && board[r][c+2] == board[r][c+3]) {
+                    setWin(r, c);
+                    return;
+                }
+            }
+        }
+    }
+    for(let r=0; r < rows-3; r++){
+        for (let c = 0; c < columns-3; c++){
+            if(board[r][c]!=' '){
+                if(board[r][c] == board[r+1][c+1] && board[r+1][c+1] == board[r+2][c+2] && board[r+2][c+2] == board[r+3][c+3]){
+                    setWin(r,c);
+                    return;
+                }
+            }
+        }
+    }
+    for(let r=3; r < rows; r++){
+        for (let c = 0; c < columns - 3; c++){
+            if(board[r][c]!=' '){
+                if(board[r][c] == board[r-1][c+1] && board[r-1][c+1] == board[r-2][c+2] && board[r-2][c+2] == board[r-3][c+3]){
+                    setWin(r,c);
+                    return;
+                }
+            }
+        }
+    }
+    for(let c = 0; c < columns; c++){
+        for(let r = 0; r < rows; r++){
+            if(board[r][c] != ' '){
+                if(board[r][c] == board[r+1][c] && board[r+1][c] == board[r+2][c] && board[r+2][c] == board[r+3][c]){
+                    setWin(r, c);
+                    return;
+                }
+            }
+        }
+    }
 }
